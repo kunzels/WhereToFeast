8d59fb65c55dbf87a8414a2578d016955718f3f4
kunzels <kunzelsteven@gmail.com>
2020-04-20 15:08:10 -0400

    FinalPrepHeroku

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const io = require("socket.io-client");
+const path = require('path');
 let socket = io.connect("http://localhost:3000");
 
 socket.on("welcome", (data) => {
@@ -30,3 +31,10 @@ app.use("/api/users", users);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
