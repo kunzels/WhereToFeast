27f4f7683260349251c84ce9af1ae8a7c28b3ad7
kunzels <kunzelsteven@gmail.com>
2020-04-20 15:45:56 -0400

    Just a small edit

--- app.js
+++ app.js
@@ -29,8 +29,6 @@ app.use(bodyParser.json());
 
 app.use("/api/users", users);
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'));
@@ -38,3 +36,6 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   })
 }
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
