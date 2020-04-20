14c50568222986c13ae02f06fe0a400031eebad6
kunzels <kunzelsteven@gmail.com>
2020-04-20 15:32:49 -0400

    Just a small edit

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(bodyParser.json());
 
 app.use("/api/users", users);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 if (process.env.NODE_ENV === 'production') {
