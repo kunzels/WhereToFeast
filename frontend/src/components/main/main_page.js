71bb39dcca52a59b558784624fe0f2f9afdca2e0
Steven Yee <s.yee615@gmail.com>
2020-04-20 11:26:30 -0400

    Skeleton

---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class MainPage extends React.Component{
+    render(){
+        return (
+          <div>
+            <h1>
+              <footer>Copyright &copy; 2020 WhereToFeast</footer>
+            </h1>
+          </div>
+        );
+    }
+}
+
+export default MainPage;
